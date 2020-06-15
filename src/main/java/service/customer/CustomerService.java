251394415875528b7c32bf074ddcc1275ab9ae6d
linhhm2406 <linh.hm24@gmail.com>
2020-06-15 17:12:46 +0700

    first init

---
+++ src/main/java/service/customer/CustomerService.java
@@ -0,0 +1,41 @@
+package service.customer;
+
+import model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import repository.ICustomerRepository;
+
+@Service
+public class CustomerService implements ICustomerService {
+
+    @Autowired
+    public ICustomerRepository customerRepository;
+
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Customer model) {
+        customerRepository.save(model);
+    }
+
+    @Override
+    public void delete(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstName, pageable);
+    }
+}
